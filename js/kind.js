dfd2f84fb84d31d47a445bc2c74241ef15d8d754
hecaiyi <164498562@.com>
2018-11-01 19:45:10 +0800

    第一次修改

---
+++ js/kind.js
@@ -0,0 +1,41 @@
+mui.plusReady(function() {
+	console.log(11111)
+	mui('.mui-bar').on('tap', '.mui-active', function() {
+		console.log(22222)
+		mui.openWindow({
+			url: 'kind.html',
+			id: 'kind.html',
+			styles: {
+			  
+			},
+			show: {
+				autoShow: true
+			},
+			waiting: {
+				autoShow: true,
+				title: '客官正在加载中，请稍后...'
+			}
+		})
+	})
+})
+
+var listNav = new Vue({
+	el: '#listNav',
+	data: {
+		list: []
+	},
+	methods: {
+		getKindList: function () {
+			var that = this;
+			mui.get('http://route.showapi.com/126-2', {
+		        showapi_appid: 79062,
+		        showapi_sign: 'cd403c88b63d4f04908edb88c4e7a670'
+		      }, function (result) {
+		      	console.log(result)
+		      })
+		}
+	},
+	created: function () {
+		 this.getKindList();
+	}
+})
